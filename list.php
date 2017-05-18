1599b27c377d1ebe8aabb348e9fe876fcabcbcf9
Shinya Hara <msc.ixb.appa.v@gmail.com>
2017-05-18 15:11:02 +0900

    重複タイトルを入力した場合、確認画面でエラーメッセージを表示し、OKボタンを無効化するように変更

--- list.php
+++ list.php
@@ -16,12 +16,7 @@ try {
   $_SESSION['flash_flag'] = true;
 }
 ?>
-<?php if (isset($_SESSION['flash_flag']) && $_SESSION['flash_flag']): ?>
-<div class="alert alert-<?=$_SESSION['status']?>" role="alert">
-  <?=$_SESSION['flash_msg']?>
-  <?php $_SESSION['flash_flag'] = false; ?>
-</div>
-<?php endif; ?>
+<?php include __DIR__.'/flash.php'; ?>
 <h3>アンケート一覧</h3>
 <table class="table">
   <thead>
