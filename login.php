3aa5dc93db90152b61fb70ec65425bfd4f56f74c
Shinya Hara <msc.ixb.appa.v@gmail.com>
2017-05-23 17:37:00 +0900

    ログインページのデザインを変更

--- login.php
+++ login.php
@@ -38,35 +38,79 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       : '$2y$10$abcdefghijklmnopqrstuv' // ユーザ名が存在しないときだけ極端に速くなるのを防ぐ
       )
     ) {
-      // 認証が成功したとき
-      // セッションIDの追跡を防ぐ
-      session_regenerate_id(true);
-      // ログイン完了後にフラッシュメッセージを表示する
-      $_SESSION['status'] = "success";
-      $_SESSION['flash_msg'] = "ようこそ，".$username."さん．";
-      $_SESSION['flash_flag'] = true;
-      // ユーザ名をセット
-      $_SESSION['username'] = $username;
-      // ログイン完了後に / に遷移
-      header('Location: /management.php');
-      exit;
-    }
-    // 認証が失敗したとき
-    // 「403 Forbidden」
-    http_response_code(403);
+    // 認証が成功したとき
+    // セッションIDの追跡を防ぐ
+    session_regenerate_id(true);
+    // ログイン完了後にフラッシュメッセージを表示する
+    $_SESSION['status'] = "success";
+    $_SESSION['flash_msg'] = "ようこそ，".$username."さん";
+    $_SESSION['flash_flag'] = true;
+    // ユーザ名をセット
+    $_SESSION['username'] = $username;
+    // ログイン完了後に /management.php に遷移
+    header('Location: /management.php');
+    exit;
   }
-  header('Content-Type: text/html; charset=UTF-8');
-  ?>
-  <!DOCTYPE html>
-  <title>ログインページ</title>
-  <h1>ログインしてください</h1>
-  <form method="post" action="">
-    ユーザ名: <input type="text" name="username" value="">
-    パスワード: <input type="password" name="password" value="">
-    <input type="hidden" name="token" value="<?=h(generate_token())?>">
-    <input type="submit" value="ログイン">
-  </form>
-  <p>ユーザ名：user パスワード：user でテストユーザとしてログイン可能</p>
-  <?php if (http_response_code() === 403): ?>
-    <p style="color: red;">ユーザ名またはパスワードが違います</p>
-  <?php endif; ?>
+  // 認証が失敗したとき
+  // 「403 Forbidden」
+  http_response_code(403);
+  $_SESSION['status'] = "danger";
+  $_SESSION['flash_msg'] = "ユーザ名またはパスワードが違います";
+  $_SESSION['flash_flag'] = true;
+}
+header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
+    <title>ログイン | アンケートシステム</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>アンケートシステム</h1><hr>
+      <!--<h2>ログイン</h2>-->
+      <!--<form method="post" action="">-->
+      <!--  <div class="form-group">-->
+      <!--    <label for="username">Name:</label>-->
+      <!--    <input type="text" class="form-control" id="username" name="username">-->
+      <!--  </div>-->
+      <!--  <div class="form-group">-->
+      <!--    <label for="password">Password:</label>-->
+      <!--    <input type="password" class="form-control" id="password" name="password">-->
+      <!--  </div>-->
+      <!--  <input type="hidden" name="token" value="<?=h(generate_token())?>">-->
+      <!--  <button type="submit" class="btn btn-primary">Login</button>-->
+      <!--</form>-->
+      
+      <?php include __DIR__.'/flash.php'; ?>
+      <div class="login-container">
+        <div class="avatar"></div>
+        <div class="form-box">
+          <form action="" method="post">
+            <input name="username" type="text" placeholder="username">
+            <input name="password" type="password" placeholder="password">
+            <input type="hidden" name="token" value="<?=h(generate_token())?>">
+            <button class="btn btn-info btn-block login" type="submit">Login</button>
+            <div class="text-center">or</div>
+            <div class="clearfix">
+              <a href="#"><button class="btn btn-primary pull-left signup" type="button">Sign Up</button></a>
+              <a href="#"><button class="btn btn-warning pull-right guest" type="button">Guest</button></a>
+            </div>
+          </form>
+        </div>
+      </div>
+      
+      <p class="text-center">ユーザ名：user パスワード：user でテストユーザとしてログイン可能</p>
+      <?php if (http_response_code() === 403): ?>
+        <!--<p style="color: red;">ユーザ名またはパスワードが違います</p>-->
+      <?php endif; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
