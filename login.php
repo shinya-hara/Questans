ac1a98b2b1fac5e644311b5b44491776257f3d43
Shinya Hara <msc.ixb.appa.v@gmail.com>
2017-05-23 17:37:00 +0900

    ログインページのデザインを変更

--- login.php
+++ login.php
@@ -38,35 +38,173 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       : '$2y$10$abcdefghijklmnopqrstuv' // ユーザ名が存在しないときだけ極端に速くなるのを防ぐ
       )
     ) {
-      // 認証が成功したとき
-      // セッションIDの追跡を防ぐ
-      session_regenerate_id(true);
-      // ログイン完了後にフラッシュメッセージを表示する
-      $_SESSION['status'] = "success";
-      $_SESSION['flash_msg'] = "ようこそ，".$username."さん．";
-      $_SESSION['flash_flag'] = true;
-      // ユーザ名をセット
-      $_SESSION['username'] = $username;
-      // ログイン完了後に / に遷移
-      header('Location: /management.php');
-      exit;
-    }
-    // 認証が失敗したとき
-    // 「403 Forbidden」
-    http_response_code(403);
+    // 認証が成功したとき
+    // セッションIDの追跡を防ぐ
+    session_regenerate_id(true);
+    // ログイン完了後にフラッシュメッセージを表示する
+    $_SESSION['status'] = "success";
+    $_SESSION['flash_msg'] = "ようこそ，".$username."さん．";
+    $_SESSION['flash_flag'] = true;
+    // ユーザ名をセット
+    $_SESSION['username'] = $username;
+    // ログイン完了後に /management.php に遷移
+    header('Location: /management.php');
+    exit;
   }
-  header('Content-Type: text/html; charset=UTF-8');
-  ?>
-  <!DOCTYPE html>
-  <title>ログインページ</title>
-  <h1>ログインしてください</h1>
-  <form method="post" action="">
-    ユーザ名: <input type="text" name="username" value="">
-    パスワード: <input type="password" name="password" value="">
-    <input type="hidden" name="token" value="<?=h(generate_token())?>">
-    <input type="submit" value="ログイン">
-  </form>
-  <p>ユーザ名：user パスワード：user でテストユーザとしてログイン可能</p>
-  <?php if (http_response_code() === 403): ?>
-    <p style="color: red;">ユーザ名またはパスワードが違います</p>
-  <?php endif; ?>
+  // 認証が失敗したとき
+  // 「403 Forbidden」
+  http_response_code(403);
+}
+header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>ログイン | アンケートシステム</title>
+    <style>
+    body{
+      /*background: #eee url(img/bg.png);*/
+    }
+    html,body{
+      position: relative;
+      height: 100%;
+    }
+    .container {
+      /*height: 100%;*/
+      /*background-color: #ecf0f1;*/
+    }
+
+.login-container{
+    position: relative;
+    width: 300px;
+    margin: 80px auto;
+    padding: 20px 40px 40px;
+    text-align: center;
+    background: #fff;
+    border: 1px solid #ccc;
+}
+
+#output{
+    position: absolute;
+    width: 300px;
+    top: -75px;
+    left: 0;
+    color: #fff;
+}
+
+#output.alert-success{
+    background: rgb(25, 204, 25);
+}
+
+#output.alert-danger{
+    background: rgb(228, 105, 105);
+}
+
+
+.login-container::before,.login-container::after{
+    content: "";
+    position: absolute;
+    width: 100%;height: 100%;
+    top: 3.5px;left: 0;
+    background: #fff;
+    z-index: -1;
+    -webkit-transform: rotateZ(4deg);
+    -moz-transform: rotateZ(4deg);
+    -ms-transform: rotateZ(4deg);
+    border: 1px solid #ccc;
+
+}
+
+.login-container::after{
+    top: 5px;
+    z-index: -2;
+    -webkit-transform: rotateZ(-2deg);
+     -moz-transform: rotateZ(-2deg);
+      -ms-transform: rotateZ(-2deg);
+
+}
+
+.avatar{
+    background-image: url("img/user.png");
+    width: 100px;height: 100px;
+    margin: 10px auto 30px;
+    border-radius: 100%;
+    border: 2px solid #aaa;
+    background-size: cover;
+}
+
+.form-box input{
+    width: 100%;
+    padding: 10px;
+    text-align: center;
+    height:40px;
+    border: 1px solid #ccc;;
+    background: #fafafa;
+    transition:0.2s ease-in-out;
+
+}
+
+.form-box input:focus{
+    outline: 0;
+    background: #eee;
+}
+
+.form-box input[type="text"]{
+    border-radius: 5px 5px 0 0;
+    text-transform: lowercase;
+}
+
+.form-box input[type="password"]{
+    border-radius: 0 0 5px 5px;
+    border-top: 0;
+}
+
+.form-box button.login{
+    margin-top:15px;
+    padding: 10px 20px;
+}
+
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>アンケートシステム</h1><hr>
+      <!--<h2>ログイン</h2>-->
+      <!--<form method="post" action="">-->
+      <!--  <div class="form-group">-->
+      <!--    <label for="username">Name:</label>-->
+      <!--    <input type="text" class="form-control" id="username" name="username">-->
+      <!--  </div>-->
+      <!--  <div class="form-group">-->
+      <!--    <label for="password">Password:</label>-->
+      <!--    <input type="password" class="form-control" id="password" name="password">-->
+      <!--  </div>-->
+      <!--  <input type="hidden" name="token" value="<?=h(generate_token())?>">-->
+      <!--  <button type="submit" class="btn btn-primary">Login</button>-->
+      <!--</form>-->
+      
+      <div class="login-container">
+        <div id="output"></div>
+        <div class="avatar"></div>
+        <div class="form-box">
+          <form action="" method="post">
+            <input name="username" type="text" placeholder="username">
+            <input name="password" type="password" placeholder="password">
+            <input type="hidden" name="token" value="<?=h(generate_token())?>">
+            <button class="btn btn-info btn-block login" type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+      
+      <p>ユーザ名：user パスワード：user でテストユーザとしてログイン可能</p>
+      <?php if (http_response_code() === 403): ?>
+        <p style="color: red;">ユーザ名またはパスワードが違います</p>
+      <?php endif; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  </body>
+</html>
