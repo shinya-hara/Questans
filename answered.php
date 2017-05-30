c089d180c9323abf841453224696612b26cea14d
Shinya Hara <msc.ixb.appa.v@gmail.com>
2017-05-31 01:44:23 +0900

    アンケートが回答済みかどうか正しく判定できていなかった問題を修正

--- answered.php
+++ answered.php
@@ -2,58 +2,63 @@
 require_once __DIR__ . '/db_info.php';
 require_once __DIR__ . '/functions.php';
 require_logined_session();
-try {
-  $dbh = new PDO($dsn, $user, $password,
-                [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                  PDO::ATTR_EMULATE_PREPARES => false ]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   try {
-    // $answered_flg = true; // 同じユーザが同じアンケートに回答済みならtrue
-    $dbh->beginTransaction();   // トランザクションの開始
-    // 同じユーザが同じアンケートに回答済みでないかチェックする
-    $stmt = $dbh->prepare("select * from answers where q_id = ? and user_id = ? limit 1");
-    $stmt->bindValue(1, (int)$_POST['q_id'], PDO::PARAM_INT);
-    $stmt->bindValue(2, (int)$_SESSION['user_id'], PDO::PARAM_INT);
-    $stmt->execute();
-    $result = $stmt->fetch();
-    $rowCount = count($result);
-    if ($rowCount == 0) {
-      // $answered_flg = false;
-    
-      // 回答情報の格納
-      $stmt = $dbh->prepare("insert into answers (q_id,user_id,answered) values (?, ?, ?)");
+    $dbh = new PDO($dsn, $user, $password,
+                  [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_EMULATE_PREPARES => false ]);
+    try {
+      // $answered_flg = true; // 同じユーザが同じアンケートに回答済みならtrue
+      $dbh->beginTransaction();   // トランザクションの開始
+      // 同じユーザが同じアンケートに回答済みでないかチェックする
+      $stmt = $dbh->prepare("select count(*) from answers where q_id = ? and user_id = ? limit 1");
       $stmt->bindValue(1, (int)$_POST['q_id'], PDO::PARAM_INT);
-      $stmt->bindValue(2, $_SESSION['user_id']);
-      date_default_timezone_set('Asia/Tokyo');  // タイムゾーンの設定
-      $stmt->bindValue(3, date("Y-m-d H:i:s", time()));   // 現在時刻を取得
+      $stmt->bindValue(2, (int)$_SESSION['user_id'], PDO::PARAM_INT);
       $stmt->execute();
-      $id = $dbh->lastInsertId();   // 最後に追加したレコードのID
+      $rowCount = $stmt->fetchColumn();
+      if ($rowCount == 0) {
+        // $answered_flg = false;
       
-      // 回答詳細の格納
-      $stmt = $dbh->prepare("insert into ans_detail (ans_id,q_num,answer) values (?, ?, ?)");
-      $stmt->bindValue(1, $id, PDO::PARAM_INT);
-      for ($i = 1; $i <= $_POST['q_cnt']; $i++) {
-        $stmt->bindValue(2, $i, PDO::PARAM_INT);
-        $stmt->bindValue(3, $_POST['a'.$i], PDO::PARAM_INT);
+        // 回答情報の格納
+        $stmt = $dbh->prepare("insert into answers (q_id,user_id,answered) values (?, ?, ?)");
+        $stmt->bindValue(1, (int)$_POST['q_id'], PDO::PARAM_INT);
+        $stmt->bindValue(2, $_SESSION['user_id']);
+        date_default_timezone_set('Asia/Tokyo');  // タイムゾーンの設定
+        $stmt->bindValue(3, date("Y-m-d H:i:s", time()));   // 現在時刻を取得
         $stmt->execute();
+        $id = $dbh->lastInsertId();   // 最後に追加したレコードのID
+        
+        // 回答詳細の格納
+        $stmt = $dbh->prepare("insert into ans_detail (ans_id,q_num,answer) values (?, ?, ?)");
+        $stmt->bindValue(1, $id, PDO::PARAM_INT);
+        for ($i = 1; $i <= $_POST['q_cnt']; $i++) {
+          $stmt->bindValue(2, $i, PDO::PARAM_INT);
+          $stmt->bindValue(3, $_POST['a'.$i], PDO::PARAM_INT);
+          $stmt->execute();
+        }
+        $dbh->commit();
+        
+        $_SESSION['status'] = "success";
+        $_SESSION['flash_msg'] = "ありがとうございます.<br>回答結果を送信しました．";
+        $_SESSION['flash_flag'] = true;
+      } else {
+        $_SESSION['status'] = "danger";
+        $_SESSION['flash_msg'] = "あなたは既にこのアンケートに回答済みです．";
+        $_SESSION['flash_flag'] = true;
       }
-      $dbh->commit();
-      
-      $_SESSION['status'] = "success";
-      $_SESSION['flash_msg'] = "ありがとうございます.<br>回答結果を送信しました．";
-      $_SESSION['flash_flag'] = true;
-    } else {
+    } catch (PDOException $e) {
       $_SESSION['status'] = "danger";
-      $_SESSION['flash_msg'] = "あなたは既にこのアンケートに回答済みです．";
+      $_SESSION['flash_msg'] = "回答結果の送信に失敗しました．";
       $_SESSION['flash_flag'] = true;
     }
   } catch (PDOException $e) {
     $_SESSION['status'] = "danger";
-    $_SESSION['flash_msg'] = "回答結果の送信に失敗しました．";
+    $_SESSION['flash_msg'] = "データベースの接続に失敗しました．";
     $_SESSION['flash_flag'] = true;
   }
-} catch (PDOException $e) {
+} else {
   $_SESSION['status'] = "danger";
-  $_SESSION['flash_msg'] = "データベースの接続に失敗しました．";
+  $_SESSION['flash_msg'] = "不正なアクセスです．";
   $_SESSION['flash_flag'] = true;
 }
 ?>
@@ -74,7 +79,7 @@ try {
       <?php include __DIR__.'/flash.php'; ?>
       <?php
       for ($i = 1; $i <= $_POST['q_cnt']; $i++) {
-        echo $i.'番の回答は'.$_POST['a'.$i].'<br>';
+        echo $i.'番の回答は 選択肢'.$_POST['a'.$i].'<br>';
       }
       ?>
       <a href="management.php" class="btn btn-default">マイページ</a>
