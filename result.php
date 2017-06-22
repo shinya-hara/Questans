2f301bc792794a383caa170a0f9cf188d0130b96
Shinya Hara <msc.ixb.appa.v@gmail.com>
2017-06-22 15:51:44 +0900

    アンケート結果をグラフ表示できるように変更

--- result.php
+++ result.php
@@ -32,17 +32,28 @@ try {
     // 回答結果を取得
     $stmt = $dbh->prepare("SELECT count(answer) FROM answers AS a, ans_detail AS d WHERE a.ans_id = d.ans_id AND q_id = ? AND q_num = ? AND answer = ?");
     $stmt->bindValue(1, (int)$_POST['q_id'], PDO::PARAM_INT); // アンケートIDをbind
-    for ($i = 1; $i <= $qCount; $i++) {
-      $stmt->bindValue(2, (int)$i, PDO::PARAM_INT);   // 質問番号をbind
-      for ($j = 1; $j <= $cCount; $j++) {
-        $stmt->bindValue(3, (int)$j, PDO::PARAM_INT); // 選択肢をbind
+    for ($i = 0; $i < $qCount; $i++) {
+      $stmt->bindValue(2, (int)$i+1, PDO::PARAM_INT);   // 質問番号をbind
+      for ($j = 0; $j < $cCount; $j++) {
+        $stmt->bindValue(3, (int)$j+1, PDO::PARAM_INT); // 選択肢をbind
         $stmt->execute();
         $result = $stmt->fetch();
-        $results_array[$i][$j] = $result['count(answer)'];  // 二次元配列に結果を格納
-        // echo $result['count(answer)'].' ';
+        $results_array[$j][$i] = $result['count(answer)'];  // 二次元配列に結果を格納
       }
-      // echo '<br>';
     }
+    // グラフ作成用のデータを格納しJSON形式でJavaScriptに渡す
+    $chart_data = [];
+    for ($i = 0; $i < $cCount; $i++) {
+      $chart_data[$i] = ['name'=>$choices[$cCount-$i-1]['choice'], 'data'=>$results_array[$cCount-$i-1]];
+    }
+    $jsonData = json_encode($chart_data);
+    // 質問番号のラベルデータを格納しJSON形式でJavaScriptに渡す
+    $categories = [];
+    for ($i = 0; $i < $qCount; $i++) {
+      $categories[$i] = '質問'.($i+1);
+    }
+    $jsonCategories = json_encode($categories);
+    
   } catch (PDOException $e) {
     $_SESSION['status'] = "danger";
     $_SESSION['flash_msg'] = "アンケート一覧の取得に失敗しました．";
@@ -76,12 +87,14 @@ try {
         <td class="td-num none"><?=$i+1?></td>
         <td class="text-left"><?=h($questions[$i]['question'])?></td>
         <?php for ($j = 0; $j < $cCount; $j++): ?>
-        <td><?=$results_array[$i+1][$j+1]?></td>
+        <td><?=$results_array[$j][$i]?></td>
         <?php endfor; ?>
       </tr>
       <?php endfor; ?>
     </tbody>
   </table>
+  <div id="chart"></div>
+  
 </div>
 <script>
   $(function() {
@@ -97,7 +110,7 @@ try {
         $('main').html(data);
       });
     });
-     // ユーザページ
+    // ユーザページ
     $('span.owner').on('click', function() {
       $.post('user.php',
       {
@@ -106,6 +119,55 @@ try {
       function(data) {
         $('main').html(data);
       });
+    });
+    // グラフ表示
+    var chartData = $.parseJSON('<?=$jsonData?>'.replace(/(\r\n)/g, '\\n'));
+    var xAxisCategories = $.parseJSON('<?=$jsonCategories?>'.replace(/(\r\n)/g, '\\n'));
+    Highcharts.setOptions({
+      // colors: ['#ff7f7f', '#ffbf7f', '#7fff7f', '#7fffff', '#bf7fff']
+      // colors: ['#ff3300', '#ffff66', '#66ff66', '#99ccff', '#cc99ff']
+      colors: ['#247BA0', '#6EA4BF', '#C2EFEB', '#3E517A', '#120D31']
+    });
+    Highcharts.chart('chart', {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'アンケート結果'
+      },
+      xAxis: {
+        categories: xAxisCategories
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: '割合 (%)'
+        },
+        reversedStacks: true,
+        minTickInterval: 10
+      },
+      // tooltip: {
+      //   pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+      //   shared: true
+      // },
+      legend: {
+        reversed: true
+      },
+      plotOptions: {
+        series: {
+          stacking: 'percent',
+          dataLabels: {
+            enabled: true,
+            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+            formatter: function() {
+              if (this.y != 0) {
+                return Math.round(this.percentage)+' %';
+              }
+            }
+          }
+        }
+      },
+      series: chartData
     });
   });
 </script>
